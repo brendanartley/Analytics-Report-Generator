9ba6a6f454709587b453beee5fef39ff9cc8bd23
brendan <baa30@sfu.ca>
2021-12-31 12:24:04 -0800

    Added API Batches + Error Prevention

--- data_gathering/api_functions.py
+++ data_gathering/api_functions.py
@@ -34,7 +34,7 @@ def get_game_ids(just_regular_season, filter_by_team, season_start, season_end, 
     responses = grequests.map(reqs)
 
     #Fetch ID's from json
-    for r in tqdm(responses, desc="Writing Data to Disk"):
+    for r in tqdm(responses, desc="Collecting List"):
         if r.ok:
             d = r.json()
             for i in range(len(d["dates"])):
@@ -57,19 +57,23 @@ def get_all_player_ids_in_season(season_start, season_end):
     urls = ["{}{}{}{}".format(url, endpoint, i, i+1) for i in range(season_start // 10000, (season_end // 10000)+1)]
     players = set()
 
-    #making requests
-    reqs = (grequests.get(u) for u in tqdm(urls, desc="API Requests"))
-    responses = grequests.map(reqs)
+    #Doing API calls in batches
+    for b in tqdm(range(0, len(urls), 100), desc="Iterating Batches"):
+        batch = urls[b:b+100]
 
-    #writing data to file
-    for r in tqdm(responses, desc="Writing Data to Disk"):
-        if r.ok:
-            d = r.json()
-            for i in range(len(d['teams'])):
-                team_data = d['teams'][i]
-                for player in team_data['roster']['roster']:
-                    if "person" in player and "id" in player["person"]:
-                        players.add(player["person"]["id"])
+        #making requests
+        reqs = (grequests.get(u) for u in batch)
+        responses = grequests.map(reqs)
+
+        #writing data to file
+        for r in responses:
+            if r.ok:
+                d = r.json()
+                for i in range(len(d['teams'])):
+                    team_data = d['teams'][i]
+                    for player in team_data['roster']['roster']:
+                        if "person" in player and "id" in player["person"]:
+                            players.add(player["person"]["id"])
 
     return list(players)
 
@@ -92,7 +96,7 @@ def simple_game_stats(fname, season_start, season_end, just_regular_season=True)
         urls = [x + "&gameType=R" for x in urls]
 
     #making requests
-    reqs = (grequests.get(u) for u in tqdm(urls, desc="API Requests"))
+    reqs = (grequests.get(u) for u in urls)
     responses = grequests.map(reqs)
 
     with open("./raw_data/{}.json".format(fname), 'w', encoding='utf-16') as f:
@@ -113,7 +117,7 @@ def simple_game_stats(fname, season_start, season_end, just_regular_season=True)
         team_info = ["id", "name"]
 
         for r in tqdm(responses, desc="Writing Data to Disk"):
-            if r.ok:
+            if r is not None and r.ok and r.json() is not None:
                 d = r.json()
 
                 #looping through every game in specified season
@@ -138,7 +142,11 @@ def simple_game_stats(fname, season_start, season_end, just_regular_season=True)
                                         rj["home_{}".format(item)] = game["linescore"]["teams"]["home"]["team"][item]
 
                                 f.write(json.dumps(rj) + '\n')
-        pass
+
+        f.seek(0, 2) # seek to end of file
+        f.seek(f.tell() - 2, 0)  # seek to the second last char of file
+        f.truncate()
+    pass
 
 def get_all_game_event_stats(game_id_array, fname):
     """
@@ -156,74 +164,73 @@ def get_all_game_event_stats(game_id_array, fname):
     #Formatting URL's
     urls = [url + endpoint.format(date) for date in game_id_array]
 
-    #Making requests - 10x faster than simple requests
-    reqs = (grequests.get(u) for u in tqdm(urls, desc="API Requests"))
-    responses = grequests.map(reqs)
-
-    #NEED TO COMPLETE FUNCTION HERE - Need to explore raw data
-    # ex - 'https://statsapi.web.nhl.com/api/v1/game/2020020663/feed/live'
-    #
-    # d["liveData"]["plays"]["allPlays"][10]
-    #  - need to find out who was on the ice for each event
-    rj = {}
-
     with open("./raw_data/{}.json".format(fname), 'w', encoding='utf-16') as f:
-        for resp in tqdm(responses, desc="Writing Data to Disk"):
-            if resp.ok:
-                d = resp.json()
+        #Doing API calls in batches
+        for b in tqdm(range(0, len(urls), 100), desc="Iterating Batches"):
+            batch = urls[b:b+100]
 
-                if "gamePk" in d:
-                    rj["gamePk"] = d["gamePk"]
-                else:
-                    continue
+            #Making requests - 10x faster than simple requests
+            reqs = (grequests.get(u) for u in batch)
+            responses = grequests.map(reqs)
 
-                for val in d["liveData"]["plays"]["allPlays"]:
+            rj = {}
 
-                    rj["event"] = val["result"]["event"]
-                    rj["periodTime"] = val["about"]["periodTime"]
-                    rj["dateTime"] = val["about"]["dateTime"]
-                    rj["period"] = val["about"]["period"]
-                    
-                    #on ice-coordinates
-                    if len(val["coordinates"]) == 2:
-                        rj["x_coordinate"] = val["coordinates"]["x"]
-                        rj["y_coordinate"] = val["coordinates"]["y"]
+            for resp in responses:
+                if resp is not None and resp.ok and resp.json() is not None:
+                    d = resp.json()
+
+                    if "gamePk" in d:
+                        rj["gamePk"] = d["gamePk"]
                     else:
-                        rj["x_coordinate"] = None
-                        rj["y_coordinate"] = None
+                        continue
 
-                    #players involved, can be up to 4
-                    if "players" in val:
-                        num_players = len(val["players"])
-                        if num_players > 4:
-                            print(" ---------- Event > 4 Players?? ---------- ")
-                        for i in range(4):
-                            if i < num_players:
-                                rj["p{}_id".format(i+1)] = val["players"][i]["player"]["id"]
-                                rj["p{}_type".format(i+1)] = val["players"][i]["playerType"]
-                                rj["p{}_name".format(i+1)] = val["players"][i]["player"]["fullName"]
-                            else:
+                    for val in d["liveData"]["plays"]["allPlays"]:
+
+                        rj["event"] = val["result"]["event"]
+                        rj["periodTime"] = val["about"]["periodTime"]
+                        rj["dateTime"] = val["about"]["dateTime"]
+                        rj["period"] = val["about"]["period"]
+                        
+                        #on ice-coordinates
+                        if len(val["coordinates"]) == 2:
+                            rj["x_coordinate"] = val["coordinates"]["x"]
+                            rj["y_coordinate"] = val["coordinates"]["y"]
+                        else:
+                            rj["x_coordinate"] = None
+                            rj["y_coordinate"] = None
+
+                        #players involved, can be up to 4
+                        if "players" in val:
+                            num_players = len(val["players"])
+                            if num_players > 4:
+                                print(" ---------- Event > 4 Players?? ---------- ")
+                            for i in range(4):
+                                if i < num_players:
+                                    rj["p{}_id".format(i+1)] = val["players"][i]["player"]["id"]
+                                    rj["p{}_type".format(i+1)] = val["players"][i]["playerType"]
+                                    rj["p{}_name".format(i+1)] = val["players"][i]["player"]["fullName"]
+                                else:
+                                    rj["p{}_id".format(i+1)] = None
+                                    rj["p{}_type".format(i+1)] = None
+                                    rj["p{}_name".format(i+1)] = None
+
+                        else:
+                            for i in range(4):
                                 rj["p{}_id".format(i+1)] = None
                                 rj["p{}_type".format(i+1)] = None
                                 rj["p{}_name".format(i+1)] = None
 
-                    else:
-                        for i in range(4):
-                            rj["p{}_id".format(i+1)] = None
-                            rj["p{}_type".format(i+1)] = None
-                            rj["p{}_name".format(i+1)] = None
-
-                    #team-id if relevant
-                    if "team" in val:
-                        rj["team_id"] = val["team"]["id"]
-                    else:
-                        rj["team_id"] = None
-                    
-                    #write output to JSON
-                    f.write(json.dumps(rj) + '\n')
-            else:
-                print("{}".format(resp.status_code))
-        
+                        #team-id if relevant
+                        if "team" in val:
+                            rj["team_id"] = val["team"]["id"]
+                        else:
+                            rj["team_id"] = None
+                        
+                        #write output to JSON
+                        f.write(json.dumps(rj) + '\n')
+                else:
+                    print("{}".format(resp.status_code))
+            
         #remove the '/n' at the end of written JSON file
         f.seek(0, 2) # seek to end of file
         f.seek(f.tell() - 2, 0)  # seek to the second last char of file
@@ -271,29 +278,34 @@ def get_players_season_goal_stats(player_id_array, season_start, season_end, fna
         for url in url_format:
             urls.append(url.format(p_id))
 
-    #making requests
-    reqs = (grequests.get(u) for u in tqdm(urls, desc="API Requests"))
-    responses = grequests.map(reqs)
-
-    #writing data to file
     with open("./raw_data/{}.json".format(fname), 'w', encoding='utf-16') as f:
-        
         rj = {}
-        for event in goal_stats_tracked:
-            rj[event] = ""
 
-        for r in tqdm(responses, desc="Writing Data to Disk"):
-            if r.ok:
-                d = r.json()
-                rj['p_id'] = int(re.findall(r'[//][0-9]+', r.url)[0][1:])
-                rj['season'] = re.findall(r'\d+', r.url)[-1]
-                if len(d["stats"][0]["splits"]) != 0:
-                    for ev in goal_stats_tracked:
-                        if ev in d["stats"][0]["splits"][0]["stat"]:
-                            rj[ev] = d["stats"][0]["splits"][0]["stat"][ev]
-                        else:
-                            rj[ev] = 0
-                    f.write(json.dumps(rj) + '\n')
+        #Doing API calls in batches
+        for b in tqdm(range(0, len(urls), 100), desc="Iterating Batches"):
+            batch = urls[b:b+100]
+
+            #making requests
+            reqs = (grequests.get(u) for u in batch)
+            responses = grequests.map(reqs)
+
+            for r in responses:
+                if r is not None and r.ok and r.json() is not None:
+                    d = r.json()
+                    rj['p_id'] = int(re.findall(r'[//][0-9]+', r.url)[0][1:])
+                    rj['season'] = re.findall(r'\d+', r.url)[-1]
+                    if len(d["stats"][0]["splits"]) != 0:
+                        for ev in goal_stats_tracked:
+                            if ev in d["stats"][0]["splits"][0]["stat"]:
+                                rj[ev] = d["stats"][0]["splits"][0]["stat"][ev]
+                            else:
+                                rj[ev] = 0
+                        f.write(json.dumps(rj) + '\n')
+
+        #remove the '/n' at the end of written JSON file
+        f.seek(0, 2) # seek to end of file
+        f.seek(f.tell() - 2, 0)  # seek to the second last char of file
+        f.truncate()
 
 def get_players_season_general_stats(player_id_array, season_start, season_end, fname, just_regular_season=True):
     """
@@ -350,28 +362,36 @@ def get_players_season_general_stats(player_id_array, season_start, season_end, 
         for url in url_format:
             urls.append(url.format(p_id))
 
-    #making requests
-    reqs = (grequests.get(u) for u in tqdm(urls, desc="API Requests"))
-    responses = grequests.map(reqs)
-
     #writing data to file
     with open("./raw_data/{}.json".format(fname), 'w', encoding='utf-16') as f:
-
         rj = {}
 
-        for r in tqdm(responses, desc="Writing Data to Disk"):
-            if r.ok:
-                d = r.json()
-                rj['p_id'] = int(re.findall(r'[//][0-9]+', r.url)[0][1:])
-                if len(d["stats"][0]["splits"]) != 0:
-                    rj["season"] = d["stats"][0]["splits"][0]['season']
-                    for ev in stats_tracked:
-                        if ev in d["stats"][0]["splits"][0]["stat"]:
-                            rj[ev] = d["stats"][0]["splits"][0]["stat"][ev]
-                        else:
-                            rj[ev] = None
-                    
-                    f.write(json.dumps(rj) + '\n')
+        #Doing API calls in batches
+        for b in tqdm(range(0, len(urls), 100), desc="Iterating Batches"):
+            batch = urls[b:b+100]
+
+            #making requests
+            reqs = (grequests.get(u) for u in batch)
+            responses = grequests.map(reqs)
+
+            for r in responses:
+                if r is not None and r.ok and r.json() is not None:
+                    d = r.json()
+                    rj['p_id'] = int(re.findall(r'[//][0-9]+', r.url)[0][1:])
+                    if len(d["stats"][0]["splits"]) != 0:
+                        rj["season"] = d["stats"][0]["splits"][0]['season']
+                        for ev in stats_tracked:
+                            if ev in d["stats"][0]["splits"][0]["stat"]:
+                                rj[ev] = d["stats"][0]["splits"][0]["stat"][ev]
+                            else:
+                                rj[ev] = None
+                        
+                        f.write(json.dumps(rj) + '\n')
+
+        #remove the '/n' at the end of written JSON file
+        f.seek(0, 2) # seek to end of file
+        f.seek(f.tell() - 2, 0)  # seek to the second last char of file
+        f.truncate()
 
 def get_players_season_stat_rankings(player_id_array, season_start, season_end, fname, just_regular_season=True):
     """
@@ -412,44 +432,55 @@ def get_players_season_stat_rankings(player_id_array, season_start, season_end, 
         for url in url_format:
             urls.append(url.format(p_id))
 
-    #making requests
-    reqs = (grequests.get(u) for u in tqdm(urls, desc="API Requests"))
-    responses = grequests.map(reqs)
-
     #writing data to file
     with open("./raw_data/{}.json".format(fname), 'w', encoding='utf-16') as f:
-        
         rj = {}
 
-        for r in tqdm(responses, desc="Writing Data to Disk"):
-            if r.ok:
-                d = r.json()
-                rj['p_id'] = int(re.findall(r'[//][0-9]+', r.url)[0][1:])
+        #Doing API calls in batches
+        for b in tqdm(range(0, len(urls), 100), desc="Iterating Batches"):
+            batch = urls[b:b+100]
 
-                if len(d["stats"][0]["splits"]) != 0:
-                    rj["season"] = d["stats"][0]["splits"][0]['season']
-                    for ev in stats_tracked:
-                        if ev in d["stats"][0]["splits"][0]["stat"]:
-                            rj[ev] = d["stats"][0]["splits"][0]["stat"][ev]
-                        else:
-                            rj[ev] = None
-                    
-                    f.write(json.dumps(rj) + '\n')
+            #making requests
+            reqs = (grequests.get(u) for u in batch)
+            responses = grequests.map(reqs)
+
+            for r in responses:
+                if r is not None and r.ok and r.json() is not None:
+                    d = r.json()
+                    rj['p_id'] = int(re.findall(r'[//][0-9]+', r.url)[0][1:])
+
+                    if len(d["stats"][0]["splits"]) != 0:
+                        rj["season"] = d["stats"][0]["splits"][0]['season']
+                        for ev in stats_tracked:
+                            if ev in d["stats"][0]["splits"][0]["stat"]:
+                                rj[ev] = d["stats"][0]["splits"][0]["stat"][ev]
+                            else:
+                                rj[ev] = None
+                        
+                        f.write(json.dumps(rj) + '\n')
+
+        #remove the '/n' at the end of written JSON file
+        f.seek(0, 2) # seek to end of file
+        f.seek(f.tell() - 2, 0)  # seek to the second last char of file
+        f.truncate()
 
 def main(output):
 
-    ss = 20112012
-    se = 20202021
+    # ss = 20112012
+    # se = 20202021
 
-    all_game_ids = get_game_ids(just_regular_season=True, filter_by_team=False, season_start=ss, season_end=se)
-    all_player_ids = get_all_player_ids_in_season(season_start=ss, season_end=ss)
+    # #get ids
+    # all_game_ids = get_game_ids(just_regular_season=True, filter_by_team=False, season_start=ss, season_end=se)
+    # all_player_ids = get_all_player_ids_in_season(season_start=ss, season_end=ss)
 
-    simple_game_stats(fname=output, season_start=ss, season_end=se, just_regular_season=True)
-    get_all_game_event_stats(all_game_ids, fname=output)
-    get_players_season_goal_stats(all_player_ids, season_start=ss, season_end=se, fname=output)
-
-    get_players_season_general_stats(all_player_ids, season_start=ss, season_end=se, fname=output)
-    get_players_season_stat_rankings(all_player_ids, season_start=ss, season_end=se, fname = output)
+    # #game events + stats
+    # simple_game_stats(fname=output, season_start=ss, season_end=se, just_regular_season=True)
+    # get_all_game_event_stats(all_game_ids, fname=output)
+    
+    # #player_stats
+    # get_players_season_goal_stats(all_player_ids, season_start=ss, season_end=se, fname=output)
+    # get_players_season_general_stats(all_player_ids, season_start=ss, season_end=se, fname=output)
+    # get_players_season_stat_rankings(all_player_ids, season_start=ss, season_end=se, fname = output)
     return
     
 if __name__ == '__main__':
